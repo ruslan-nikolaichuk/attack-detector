f89eab24d617cbeb3a0e92ebee594d1f96108d6f
ruslan nikolaichuk <ruslan.nikolychuk@gmail.com>
2023-01-23 23:19:25 +0200

    init

---
+++ src/main/java/io/httpflood/attackdetector/HttpfloodAttackDetectorApplication.java
@@ -0,0 +1,13 @@
+package io.httpflood.attackdetector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HttpfloodAttackDetectorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HttpfloodAttackDetectorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/httpflood/attackdetector/HttpfloodAttackDetectorApplicationTests.java
@@ -0,0 +1,13 @@
+package io.httpflood.attackdetector;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HttpfloodAttackDetectorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
